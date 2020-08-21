568e20a9b503da2335b7f0ad7479caa27592d0ce
chnovak <67983461+chnovak@users.noreply.github.com>
2020-08-21 12:52:35 -0500

    changes to js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getData() {
     $(".inner").empty();
     var input = $('#search').val();
-    $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=KGu4zQMiVhxF5VLjrBolQiJzyLardbaM&limit=30', function (response) {
+    $.get('http://api.giphy.com/v1/gifs/search?q='+ input +'+&api_key=KGu4zQMiVhxF5VLjrBolQiJzyLardbaM&limit=30', function (response) {
 
         for (i = 0; response.data.length; i++) {
        $('.inner').append("<img src=" + response.data[i].images.original.url +">")
