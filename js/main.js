73b9bb1b46aa4d08879dc6ae4b7964a66d27cc71
chnovak <67983461+chnovak@users.noreply.github.com>
2020-08-21 12:24:50 -0500

    made more changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-function getData(){
-    var input = $("#searchtext").val()
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=KGu4zQMiVhxF5VLjrBolQiJzyLardbaM&limit=5");
-xhr.done(function(response) { 
-    console.log("success got data", response); 
+function getData() {
+    $(".inner").empty();
+    var input = $('#search').val();
+    $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=KGu4zQMiVhxF5VLjrBolQiJzyLardbaM&limit=30', function (response) {
 
-    var jiffs = response.getData
-
-    for (i in jiffs)
-    {
-        $('.inner').append("<img src='"+jiffs[i].images.original.url+"' style='height:350px; width:350px;'/>")
+        for (i = 0; response.data.length; i++) {
+       $('.inner').append("<img src=" + response.data[i].images.original.url +">")
     }    
 
-});
+})
+$div.addClass("gif-box");
 
 }
